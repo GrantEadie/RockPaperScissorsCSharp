7e83e7ac7ac9ee4d9e4ad7f8c0369ba377e989bd
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-23 16:29:50 -0700

    add a random number generator to have a person play against a computer

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,21 +4,50 @@ using RockPaperScissors.Models;
 
 namespace RockPaperScissors.Models
 {
-  public class Program 
+  public class Program
   {
     public static void Main()
     {
       Console.WriteLine("WELCOME TO THE GREATEST GAME EVER MADE.");
-      Console.WriteLine("Player 1, choose you weapon. Rock, Paper, or Scissors: ");
-      string player1Selection = Console.ReadLine().ToLower();
-      Console.WriteLine("Player 2, choose you weapon. Rock, Paper, or Scissors: ");
-      string player2Selection = Console.ReadLine().ToLower();
+      Console.WriteLine("Would you like to play against the computer? Type 'y' for yes, 'n' for no.");
+      string choice = Console.ReadLine().ToLower();
+
+      string player1Selection = "";
+      string player2Selection = "";
+
+      if (choice == "y")
+      {
+        Console.WriteLine("Player 1, choose you weapon. Rock, Paper, or Scissors: ");
+        player1Selection = Console.ReadLine().ToLower();
+        Random rnd = new Random();
+        int computer = rnd.Next(1, 4);
+        if (computer == 1)
+        {
+          player2Selection = "rock";
+        }
+        else if (computer == 2)
+        {
+          player2Selection = "scissors";
+        }
+        else 
+        {
+          player2Selection = "paper";
+        }
+        Console.WriteLine("Computer chose: " + player2Selection);
+      }
+      else
+      {
+        Console.WriteLine("Player 1, choose you weapon. Rock, Paper, or Scissors: ");
+        player1Selection = Console.ReadLine().ToLower();
+        Console.WriteLine("Player 2, choose you weapon. Rock, Paper, or Scissors: ");
+        player2Selection = Console.ReadLine().ToLower();
+      }
 
       if (player1Selection == player2Selection)
       {
         player1Selection = player1Selection + "1";
       }
-      
+
       Game mainGame = new Game();
       string output = mainGame.PlayGame(player1Selection, player2Selection);
       Console.WriteLine(output);
